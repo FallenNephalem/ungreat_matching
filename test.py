c8c32c67e482684794c605a1efbec72ca8db89ad
FallenNephalem <ldr200@mail.ru>
2020-02-14 21:36:56 +0300

    some buns

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from ungreat_matching import ungreat_match
+from ungreat_matching import ungreat_match, num_match_max, num_match_min
 
 
-test_data = [['python','Python'],['python','python3.7'],['PYTHON','python3.7']]
+test_data = [['python','Python'],
+['python','python3.7'],
+['PYTHON','python3.7'],
+['PYTHON','python3.7.3']]
 for data in test_data:
     print(ungreat_match(data[0],data[1]))
--- ungreat_matching.py
+++ ungreat_matching.py
@@ -1,12 +1,18 @@
 import math
 #from fuzzywuzzy import fuzz
 #from fuzzywuzzy import process
+
+
+# settings
+
 alfabet = [['q','Q'],['w','W'],['e','E'],['r','R'],
 ['t','T'],['y','Y'],['u','U'],['i','I'],['o','O'],
 ['p','P'],['a','A'],['s','S'],['d','D'],['f','F'],
 ['g','G'],['h','H'],['j','J'],['k','K'],['l','L'],
 ['z','Z'],['x','X'],['c','C'],['v','V'],['b','B'],
 ['n','N'],['m','M']]
+mkdefolt=0.7
+mismatching_chars=5
 
 
 def num_match_max(num1, num2):
@@ -44,7 +50,7 @@ def ungreat_match(str1,str2):
 	if str1 == str2:
 		keys = [str1, 'True']
 		return keys
-	elif math.fabs(lenth1 - lenth2) > 3:
+	elif math.fabs(lenth1 - lenth2) > mismatching_chars:
 		test = 1
 		keys = ['mismatching skills', 'False', test]
 		return keys
@@ -64,4 +70,3 @@ def ungreat_match(str1,str2):
 			test = 2
 			keys = ['mismatching skills', 'False',test]
 			return keys
-	
