1af5ed5c82e5c6fa237255d14e9e6071bd1610cf
FallNeph <lyubashevskiy_dr_0818@1511.ru>
2020-05-01 19:23:34 +0300

    1.1.0

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='unmatch',
-      version='1.0.0',
-      description='ungreat matching of strings module',
-      packages=['unmatch'],
-      author_email='ldr200@mail.ru',
-      zip_safe=False)
